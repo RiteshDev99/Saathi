7d6fb458111eba56db329440b2930a6379e9ffc4
riteshchaudhary <riteshchaudhary5191@gmail.com>
2026-02-22 22:55:03 +0530

    refactor layout and index components for improved navigation structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,10 +10,9 @@ export default function Layout() {
        <Provider store={store}>
             <StatusBar backgroundColor="#000" barStyle="dark-content" />
             <Stack>
-                {/*<Stack.Screen name="(auth)" options={{ headerShown: false }} />*/}
-                {/*<Stack.Screen name="(drawer)" options={{ headerShown: false, headerBackTitle:'Back'  }} />*/}
-                <Stack.Screen name="(drawer)" options={{ headerShown: false, headerBackTitle:'Back'  }} />
                 <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                <Stack.Screen name="(drawer)" options={{ headerShown: false, headerBackTitle:'Back'  }} />
+
             </Stack>
        </Provider>
         </>
